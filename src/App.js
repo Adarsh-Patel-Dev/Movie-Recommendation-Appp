11ac99ab7ab5378f2d6c7258f5c9f2b7fb69d107
Adarsh Patel <81930207+Adarsh-Patel-Dev@users.noreply.github.com>
2021-09-22 18:44:39 +0530

    Added footer and bg image

--- src/App.js
+++ src/App.js
@@ -4,36 +4,36 @@ import "./styles.css";
 export default function App() {
   const recommendations = {
     Horror: [
-      { name: "A Quiet Place", release: "2018" },
-      { name: "Paranormal Activity", release: "2007" },
-      { name: "The Babadook", release: "2014" },
-      { name: "The Birds", release: "1963" }
+      { name: "A Quiet Place", release: "2018", rating: "4.3 / 5" },
+      { name: "Paranormal Activity", release: "2007", rating: "4.5 / 5" },
+      { name: "The Babadook", release: "2014", rating: "4 / 5 " },
+      { name: "The Birds", release: "1963", rating: "4.1 / 5" }
     ],
     "Sci-Fi": [
-      { name: "Xtinction", release: "2018" },
-      { name: "Coma", release: "2019" },
-      { name: "Tenet", release: "2020" },
-      { name: "Venom", release: "2018" }
+      { name: "Xtinction", release: "2018", rating: "4.3 / 5" },
+      { name: "Coma", release: "2019", rating: "4.4 / 5" },
+      { name: "Tenet", release: "2020", rating: "4.1 / 5" },
+      { name: "Venom", release: "2018", rating: "4.6 / 5" }
     ],
 
     Romantic: [
-      { name: "Remember Me", release: "2010" },
-      { name: "The Notebook", release: "2004" },
-      { name: "Twilight", release: "2008" },
-      { name: "One Day", release: "2011" }
+      { name: "Remember Me", release: "2010", rating: "4.5 / 5" },
+      { name: "The Notebook", release: "2004", rating: "4.7 / 5" },
+      { name: "Twilight", release: "2008", rating: "4.1 / 5" },
+      { name: "One Day", release: "2011", rating: "4.3 / 5" }
     ],
 
     Comedy: [
-      { name: "Due Date", release: "2010" },
-      { name: "Shazam", release: "2019" },
-      { name: "Johny English", release: "2008" },
-      { name: "Despicable Me", release: "2010" }
+      { name: "Due Date", release: "2010", rating: "4.5 / 5" },
+      { name: "Shazam", release: "2019", rating: "4.6 / 5" },
+      { name: "Johny English", release: "2008", rating: "4.7 / 5" },
+      { name: "Despicable Me", release: "2010", rating: "4.8 / 5" }
     ],
     Action: [
-      { name: "The Avengers", release: "2012" },
-      { name: "Avengers Endgame", release: "2019" },
-      { name: "Logan", release: "2017" },
-      { name: "John Wick", release: "2014" }
+      { name: "The Avengers", release: "2012", rating: "4.7 / 5" },
+      { name: "Avengers Endgame", release: "2019", rating: "4.8 / 5" },
+      { name: "Logan", release: "2017", rating: "4.5 / 5" },
+      { name: "John Wick", release: "2014", rating: "4.3 / 5" }
     ]
   };
 
@@ -50,25 +50,29 @@ export default function App() {
       <h1>Movies Library</h1>
       <p>
         {" "}
-        Movie recommendation app which recommends movies in various category
+        Movie recommendation app which recommends movies in various categories.
       </p>
-      {/* <h5>category</h5> */}
+
       <br />
-      <hr />
-      <div>
+      {/* <hr /> */}
+      <div className="category">
+        <h3>Category</h3>
         {Object.keys(recommendations).map((item) => {
           return <button onClick={() => clickHandler(item)}>{item}</button>;
         })}
-        <hr />
+
+        {/* <hr /> */}
       </div>
 
       <div>
+        {/* <br/> */}
         {recommendation.length > 0 && (
-          <p>
+          <p className="recom">
             Here are some recommendations for <strong>{category}</strong>{" "}
             Movies:
           </p>
         )}
+        <br />
         <ul>
           {recommendation.map((item) => {
             return (
@@ -76,12 +80,35 @@ export default function App() {
                 <li>
                   <div className="MovieName">{item.name}</div>
                   <div className="Release-year">{item.release}</div>
+                  <div className="Rating">{item.rating}</div>
                 </li>
               </React.Fragment>
             );
           })}
         </ul>
       </div>
+
+      <footer>
+        <h3>Social Media Presence</h3>
+        <div class="icon">
+          <a
+            href="https://github.com/Adarsh-Patel-Dev"
+            class="fa fa-github"
+          ></a>
+          <a
+            href="https://www.linkedin.com/in/adarsh-patel-449a55175"
+            class="fa fa-linkedin"
+          ></a>
+          <a
+            href="https://twitter.com/AdarshPatelDev?s=08"
+            class="fa fa-twitter"
+          ></a>
+          <a
+            href="https://instagram.com/git_sit_code"
+            class="fa fa-instagram"
+          ></a>
+        </div>
+      </footer>
     </div>
   );
 }
